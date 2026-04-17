8ae4fc427e7601605b2d1d470a5a843033d346fb
Alejandro <jorge.parrab@campusucc.edu.co>
2026-04-17 14:36:09 -0500

    first commit

---
+++ src/main/java/com/ale/observability/MicroserviceObservabilityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ale.observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceObservabilityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceObservabilityBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ale/observability/MicroserviceObservabilityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ale.observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceObservabilityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
